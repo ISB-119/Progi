e14485fc1cfa014bc4838f71a71fd37ff02afcbc
mbluuka <50544822+SashkaGitarist@users.noreply.github.com>
2019-10-14 14:43:06 +0300

    Create f.c

---
+++ f.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+#include <libm.a>
+#include <math.h>
+main()
+{
+double a, f;
+printf("Введите а\n");
+scanf("%lf", &a);
+f=pow(a,2);
+printf("%f\n",f);
+}
+
+
